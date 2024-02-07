b758f453e39501a588cc61f13a440f4ca3dd1595
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2024-02-07 12:40:30 +0100

    maps working

--- app/src/main/java/com/example/mapmarkit/MainActivity.kt
+++ app/src/main/java/com/example/mapmarkit/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_map, R.id.navigation_dashboard, R.id.navigation_settings
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
